918e41e074fbbcbef0b2f24c55c579b16a2ce61c
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-03-13 00:17:54 +0200

    adding form validation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-// === NAV MENU
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-
-//    Reveal Menu 
-      if(navToggle){
-        navToggle.addEventListener('click', () =>{
-            navMenu.classList.add('show-menu')
-        })
-    }
-
-    // Menu hidden
-    if(navClose){
-        navClose.addEventListener('click', () =>{
-            navMenu.classList.remove('show-menu')
-        })
-    }
-    
-    // Remove Menu 
-    const navLink = document.querySelectorAll('.nav__link')
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
-
 // Theme
 document.addEventListener("DOMContentLoaded", function() {
     const themeToggle = document.getElementById("theme-toggle");
@@ -40,23 +9,145 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-// Section active link 
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
+//Nav Menu
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('#nav-menu a[href*=' + sectionId + ']')
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-window.addEventListener('scroll', scrollActive)
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+// Form Validation
+
+function validateEmail(email) {
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
+
+function validateForm(event) {
+    event.preventDefault();
+
+    var names = document.getElementById("names").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var message = document.getElementById("message").value.trim();
+    var isValid = true;
+
+    // Reset previous errors and styles
+    document.getElementById("namesError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+    document.getElementById("names").classList.remove("invalid-input");
+    document.getElementById("email").classList.remove("invalid-input");
+    document.getElementById("message").classList.remove("invalid-input");
+
+    
+
+    // Validating names
+    if (names === "") {
+        document.getElementById("namesError").textContent = "Name(s) is required.";
+        document.getElementById("namesError").style.display = "block"; // Display error message
+        document.getElementById("names").classList.add("invalid-input");
+        isValid = false;
+    } else if (names.length < 3 || !/^[a-zA-Z\s]*$/.test(names)) {
+        document.getElementById("namesError").textContent = "Please enter a valid name with at least 3 characters.";
+        document.getElementById("namesError").style.display = "block"; // Display error message
+        document.getElementById("names").classList.add("invalid-input");
+        isValid = false;
+    } else if (names.length > 50) {
+        document.getElementById("namesError").textContent = "Name(s) should not exceed 50 characters.";
+        document.getElementById("namesError").style.display = "block"; // Display error message
+        document.getElementById("names").classList.add("invalid-input");
+        isValid = false;
+    }
+
+    // Validating email
+    if (email === "") {
+        document.getElementById("emailError").textContent = "Please enter your email.";
+        document.getElementById("emailError").style.display = "block"; // Display error message
+        document.getElementById("email").classList.add("invalid-input");
+        isValid = false;
+    } else if (!validateEmail(email)) {
+        document.getElementById("emailError").textContent = "Please enter a valid email address.";
+        document.getElementById("emailError").style.display = "block"; // Display error message
+        document.getElementById("email").classList.add("invalid-input");
+        isValid = false;
+    } else if (email.length > 100) {
+        document.getElementById("emailError").textContent = "Email should not exceed 100 characters.";
+        document.getElementById("emailError").style.display = "block"; // Display error message
+        document.getElementById("email").classList.add("invalid-input");
+        isValid = false;
+    }
+    // Validating Message
+    if (message === "") {
+        document.getElementById("messageError").textContent = "Message can be empty.";
+        document.getElementById("messageError").style.display = "block"; // Display error message
+    } else if (message.length < 20) {
+        document.getElementById("messageError").textContent = "Message is too short. Please enter at least 20 characters.";
+        document.getElementById("messageError").style.display = "block"; // Display error message
+        document.getElementById("message").classList.add("invalid-input");
+        isValid = false;
+    } else if (message.length > 1000) {
+        document.getElementById("messageError").textContent = "Message should not exceed 1000 characters.";
+        document.getElementById("messageError").style.display = "block"; // Display error message
+        document.getElementById("message").classList.add("invalid-input");
+        isValid = false;
+    }
+
+// When all validation passes
+let submissions = [];
+
+if (isValid) {
+    let submission = [
+        {
+            names: names,
+            email: email,
+            message: message,
+        },
+    ];
+
+  
+    submissions.push(submission);
+
+  
+    console.log("Submitted Input:", submission);
+
+    // confirmation message
+    let confirmMessage = document.getElementById("messageSent");
+    confirmMessage.style.display = "block";
+    confirmMessage.textContent = "Message sent!";
+
+    setTimeout(function() {
+        confirmMessage.style.display = "none";
+    }, 3000);
+
+    // Resetting form
+    document.getElementById("contactForm").reset();
+}
+}
